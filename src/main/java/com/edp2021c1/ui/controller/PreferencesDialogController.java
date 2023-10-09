b9185865b2d3e9aef59d2f96a56d3145a884c4da
Calboot <113321314+Calboot@users.noreply.github.com>
2023-10-09 20:27:15 +0800

    功能做完力（喜 接下来开始写CSS

--- src/main/java/com/edp2021c1/ui/controller/PreferencesDialogController.java
+++ src/main/java/com/edp2021c1/ui/controller/PreferencesDialogController.java
@@ -2,12 +2,19 @@ package com.edp2021c1.ui.controller;
 
 import com.edp2021c1.Main;
 import com.edp2021c1.core.SeatConfig;
+import com.google.gson.Gson;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 import java.util.ResourceBundle;
 
 public class PreferencesDialogController {
@@ -17,6 +24,9 @@ public class PreferencesDialogController {
 
     @FXML
     private URL location;
+
+    @FXML
+    private Stage stage;
 
     @FXML
     private TextField columnCountInput;
@@ -53,8 +63,40 @@ public class PreferencesDialogController {
     }
 
     @FXML
-    void loadConfigFromFile(ActionEvent event) {
+    void loadConfigFromFile(ActionEvent event) throws IOException {
+        FileChooser fc=new FileChooser();
+        fc.setTitle("加载配置文件");
+        fc.getExtensionFilters().add(new FileChooser.ExtensionFilter("Json文件","*.json"));
+        File f=fc.showOpenDialog(stage);
+        if (f==null) {
+            return;
+        }
 
+        FileInputStream inputStream = new FileInputStream(f);
+        byte[] bytes = new byte[inputStream.available()];
+        inputStream.read(bytes);
+        inputStream.close();
+        String str = new String(bytes, StandardCharsets.UTF_8);
+        SeatConfig seatConfig = new Gson().fromJson(str, SeatConfig.class);
+
+        rowCountInput.setText(seatConfig.rows);
+        columnCountInput.setText(seatConfig.columns);
+        rbrInput.setText(seatConfig.random_between_rows);
+        lastRowPosInput.setText(seatConfig.last_row_pos_can_be_choosed);
+        nameListInput.setText(seatConfig.person_sort_by_height);
+        groupLeaderListInput.setText(seatConfig.zz);
+        separateListInput.setText(seatConfig.separate);
+    }
+
+    @FXML
+    void confirm(ActionEvent event){
+        applySeatConfig(null);
+        stage.close();
+    }
+
+    @FXML
+    void cancel(ActionEvent event){
+        stage.close();
     }
 
     @FXML
