cabc11a2790cd70b75a616ff923583aa8912fa99
Calboot <113321314+Calboot@users.noreply.github.com>
2023-10-29 17:51:18 +0800

    更新JavaDoc

--- src/main/java/com/edp2021c1/core/IllegalSeatConfigException.java
+++ src/main/java/com/edp2021c1/core/IllegalSeatConfigException.java
@@ -21,13 +21,13 @@ public class IllegalSeatConfigException extends RuntimeException {
      * cause.
      *
      * <p>Note that the detail message associated with {@code cause} is
-     * <i>not</i> automatically incorporated in this exception's detail
+     * <i>not</i> automatically incorporated in the detail of this exception
      * message.
      *
      * @param message the detail message (which is saved for later retrieval
      *                by the {@link Throwable#getMessage()} method).
      * @param cause   the cause (which is saved for later retrieval by the
-     *                {@link Throwable#getCause()} method).  (A {@code null} value
+     *                {@link Throwable#getCause()} method). (A {@code null} value
      *                is permitted, and indicates that the cause is nonexistent or
      *                unknown.)
      */
