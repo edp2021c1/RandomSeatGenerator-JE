32b5a4a532f77ef89037cad5aae3532d5804c96e
Calboot <113321314+Calboot@users.noreply.github.com>
2023-11-16 18:02:54 +0800

    添加版权声明

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2023  EDP2021C1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 plugins {
     id("java")
 }
@@ -12,14 +29,20 @@ repositories {
 }
 
 dependencies {
+    // EasyExcel，用于导出座位表
     implementation("com.alibaba:easyexcel:3.3.2")
+
+    // EasyExcel不加这个就会报错。。。
+    implementation("org.slf4j:slf4j-simple:2.0.5")
+
+    // Gson，用于读取配置文件
     implementation("com.google.code.gson:gson:2.10.1")
-    implementation("org.slf4j:slf4j-simple:2.0.5") // EasyExcel不加这个就会报错。。。
 
+    // Lombok，主要为了 @Getter 注解，反正不占空间
     compileOnly("org.projectlombok:lombok:1.18.28")
-
     annotationProcessor("org.projectlombok:lombok:1.18.28")
 
+    // JUnit，测试用
     testImplementation("junit:junit:4.13.2")
 }
 
