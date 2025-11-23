12754642ff81dad8af2b5cad4f4e5f86b3a3400c
Calboot <calboot39@outlook.com>
2025-11-23 10:46:12 +0800

    Bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -132,8 +132,8 @@ tasks.testClasses.get().dependsOn(tasks.licenseFormatTest)
 
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
-    sourceCompatibility = "17"
-    targetCompatibility = "17"
+    sourceCompatibility = "21"
+    targetCompatibility = "21"
 }
 
 tasks.jar {
